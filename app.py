8d5e8acfb0f7984132833cf18ec3665bf7461fc4
Nazeer Akbar Hussain <nazeeronline@gmail.com>
2020-09-29 15:42:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,4 +8,4 @@ def hello():
             '<b>Hostname:</b> {hostname}<br/>'
      return html.format(name=os.getenv('NAME', 'world'), hostname=socket.gethostname())
 if __name__ == '__main__':
-     app.run(host='0.0.0.0', port=80)
+     app.run(host='0.0.0.0', port=8080)
